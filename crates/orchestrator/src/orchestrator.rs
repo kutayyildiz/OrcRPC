1668647104b2267113cfbf94bc618aec93077cfe
Kutay YILDIZ <mail@kutayyildiz.com>
2026-05-15 16:36:28 +0300

    refactor: rename Orchestrator::handle to call

--- crates/orchestrator/src/orchestrator.rs
+++ crates/orchestrator/src/orchestrator.rs
@@ -1,12 +1,17 @@
-use actrpc_core::json_rpc::JsonRpcMessage;
-use actrpc_transport::TransportTarget;
+use crate::external_method::MethodName;
+use actrpc_core::json_rpc::{JsonRpcMessage, JsonRpcParams};
+use std::{future::Future, pin::Pin};
+
+pub type OrchestratorFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'a>>;
 
 pub trait Orchestrator {
     type Error;
 
-    fn handle(
-        &self,
-        message: JsonRpcMessage,
-        destination: TransportTarget,
-    ) -> Result<JsonRpcMessage, Self::Error>;
+    fn call<'a>(
+        &'a self,
+        method: MethodName,
+        params: Option<JsonRpcParams>,
+    ) -> OrchestratorFuture<'a, Result<JsonRpcMessage, Self::Error>>
+    where
+        Self: 'a;
 }
